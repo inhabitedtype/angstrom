499f3b606745bfc7470617d0a7219b2d76c29635
Spiros Eliopoulos <spiros@inhabitedtype.com>
2016-03-04 12:50:49 -0500

    implementations of HTTP and JSON parsers

---
+++ rfcs/rFC2616.ml
@@ -0,0 +1,130 @@
+open Angstrom
+
+module P = struct
+  let is_space c =
+    c = ' ' || c = '\t'
+  let is_eol c = c = '\r' || c = '\n'
+  let is_digit c =
+    let i = Char.code c in
+    0x30 <= i & i <= 0x39
+  let is_hex c =
+    let i = Char.code c in
+    (0x30 <= i && i <= 0x39)
+    || (0x41 <= i && i <= 0x46)
+    || (0x61 <= i && i <= 0x66)
+  let is_colon c =
+    c = ':'
+  let is_colon_or_space c =
+    is_space c || is_colon c
+
+  let is_separator c = String.contains "()<>@,;:\\\"/[]?={} \t" c
+end
+
+let token = satisfy (fun c -> c <> 127 && c > 31 && not (is_separator c))
+let digits = take_while1 P.is_digit
+let separator = satisfy P.is_separator
+let spaces = skip_while P.is_space
+let hex str =
+  try return (Int64.of_string ("0x" ^ str)) with _ -> fail "hex"
+
+let lex p = p <* spaces
+
+let version =
+  string "HTTP/" *> begin
+  (fun major minor -> major, minor)
+    <$> (digits <* char '.')
+    <*> digits
+  end
+
+let uri =
+  (fun uri -> try Uri.of_string uri with _ -> fail "uri")
+    <$> take_till P.is_space
+
+let meth = lex token
+let eol = end_of_line <* commit
+
+let request_first_line =
+  (fun meth uri version -> (meth, uri, version)
+    <$> lex meth
+    <*> lex uri
+    <*> lex version
+
+let response_first_line =
+  (fun version status msg -> (version, status, msg))
+    <$> lex version
+    <*> take_till P.is_space
+    <*> take_till P.is_eol
+
+let header =
+  let colon = spaces *> char ':' <* spaces in
+  (fun key value -> (key, value))
+    <$> take_till P.is_colon_or_space
+    <*> take_till P.is_eol
+
+let request =
+  (fun (meth, uri, version) headers -> (meth, uri, version, headers))
+    <$> (request_first_line   <* eol)
+    <*> (many (header <* eol) <* eol)
+
+let response =
+  (fun (version, status, msg) headers -> (vesion, status, msg, headers))
+   <$> (response_first_line   <* eol)
+   <*> (many (header <* eol)  <* eol)
+
+module K = struct
+  let fixed_body ?size len k =
+    let rec loop len =
+      if len = 0 then
+        return ()
+      else
+        begin match size with
+        | None      -> available >>| fun size -> min len size
+        | Some size -> return (min len size)
+        end
+        >>= fun size -> take size
+        >>= fun buf  ->
+          k buf;
+          commit *> loop (len - size)
+    in
+    loop len
+
+  let chunked_body ?size k =
+    let chunk =
+      take_while1 P.is_hex <* semi
+      >>= fun s -> hex s   <* commit
+      >>= fun size ->
+        if size = 0L
+          then many skip_line *> return ()
+          else fixed_body size k <* end_of_line
+    in
+    many1 (chunk *> commit)
+
+  let unknown_body k : unit t =
+    let chunk =
+      want_input
+      >>= function
+        | true  -> available >>= fun n -> take n >>| k
+        | false -> fail "chunk"
+    in
+    (* XXX(seliopou): better make sure this isn't used with a keep-alive
+     * connection. *)
+    many (chunk <* commit) *> end_of_input *> commit
+
+  let body ?size encoding k : unit t =
+    match encoding with
+    | `Unknown   -> unknown_body k
+    | `Fixed len -> fixed_body ?size len k
+    | `Chunked   -> chunked_body ?size k
+
+  let accum_body p =
+    let chunks = ref [] in
+    let k c = chunks := c :: !chunks in
+    p k >>| fun () ->
+      match !chunks with
+      | []  -> `Empty
+      | [c] -> `String c
+      | cs  -> `Strings (List.rev cs)
+end
+
+let body ?size encoding =
+  K.accum_body (K.body ?size encoding)
---
+++ rfcs/rFC7159.ml
@@ -0,0 +1,85 @@
+type t =
+  [ `Null
+  | `False
+  | `True
+  | `String of string
+  | `Number of float
+  | `Object of (string * t) list
+  | `Array of t list ]
+
+let ws = take_while (fun c ->
+  c = '\x20' || c = '\x0a' || c = '\x0d' || c = '\x09')
+
+let lchar c =
+  ws *> char c <* ws
+
+let lsb, rsb = lchar '\x5b', lchar '\x5d'
+let lcb, rcb = lchar '\x7b', lchar '\x7d'
+let ns , vs  = lchar '\x3a', lchar '\x2c'
+let quo = lchar '\x22'
+let esc = lchar '\x5c'
+
+let _false = string "false" *> return `False
+let _true  = string "true"  *> return `True
+let _null  = string "null"  *> return `Null
+
+let num =
+  let str = take_while (fun c ->
+    c <> '\x20' && c <> '\x0a' && c <> '\x0d' && c <> '\x09'
+    && c <> '\x5d' && c <> '\x7d' && c <> '\x3a' && c <> '\x2c')
+  in
+  str
+  >>= function
+    | "" -> fail "no input for number"
+    | s  -> return (`Number (float_of_string s))
+
+let hex =
+  satisfy (fun c ->
+    let b = Char.code c in
+    (0x30 <= b && b <= 0x39) || (0x41 <= b && b <= 0x46) || (0x61 <= b && b <= 66))
+
+let _str =
+  let unescaped =
+    (* not '"' or '\' *)
+    take_while (fun c -> c <> '\x22' && c <> '\x5c') <?> "unescaped char"
+  in
+  let escaped =
+    esc *>  choice [
+      char '\x22' *> return "\x22"
+    ; char '\x5c' *> return "\x5c"
+    ; char '\x2f' *> return "\x2f"
+    ; char '\x62' *> return "\x08"
+    ; char '\x66' *> return "\x0c"
+    ; char '\x6e' *> return "\x0a"
+    ; char '\x72' *> return "\x0d"
+    ; char '\x74' *> return "\x09"
+    ; char '\x75' *> begin
+      (fun a b c d ->
+        Printf.sprintf "%c%c"
+          Char.(chr (0x10 * (code a) + (code b)))
+          Char.(chr (0x10 * (code c) + (code d))))
+        <$> hex <*> hex <*> hex <*> hex
+      end
+    ] <?> "escaped char"
+  in
+  let chars =
+    many (choice [unescaped; escaped] <* commit) >>| String.concat ""
+  in
+  quo *> chars <* quo
+
+let str =
+  (_str >>| fun s -> `String s) <?> "str"
+
+let value =
+  fix (fun v ->
+    let obj =
+      let member = return (fun x y -> (x, y)) <*> _str <* ns <*> v in
+      lcb *> sep_by member vs <* rcb >>| fun ms -> `Object ms
+    in
+    let arr =
+      lsb *> sep_by v vs <* rsb >>| fun vs -> `Array vs
+    in
+    choice [_false; _null; _true; obj; arr; str; num])
+
+let parser =
+  ws *> value <* ws
