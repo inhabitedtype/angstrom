d0aaeeffdd0960f8107315f611c5fb98e716e46a
Spiros Eliopoulos <seliopou@gmail.com>
2017-09-08 09:36:03 +0000

    fix-benchmarks: update to latest interface

--- benchmarks/pure_benchmark.ml
+++ benchmarks/pure_benchmark.ml
@@ -8,17 +8,19 @@ let read file =
   let buf  = String.create size in
   let rec loop pos len fd =
     let n = read ~pos ~len ~buf fd in
-    if n = 0 then buf else loop (pos + n) (len - n) fd
+    if n > 0 then loop (pos + n) (len - n) fd
   in
   with_file ~mode:[O_RDONLY] file ~f:(fun fd ->
-    loop 0 size fd)
+    loop 0 size fd);
+  Bigstring.of_string buf
 
 let zero =
-  String.make (65_536) '\x00'
+  let len = 65_536 in
+  Bigstring.of_string (String.make len '\x00')
 
 let make_endian name p =
   Bench.Test.create ~name (fun () ->
-    match Angstrom.(parse_only (skip_many p) (`String zero)) with
+    match Angstrom.(parse_bigstring (skip_many p) zero) with
     | R.Ok _ -> ()
     | R.Error err -> failwith err)
 
@@ -35,11 +37,11 @@ let main () =
   let http_get    = read "benchmarks/data/http-requests.txt.100" in
   Command.run (Bench.make_command [
     Bench.Test.create ~name:"json" (fun () ->
-      match Angstrom.parse_only RFC7159.json (`String big_twitter) with
+      match Angstrom.parse_bigstring RFC7159.json big_twitter with
       | R.Ok _ -> ()
       | R.Error err -> failwith err);
     Bench.Test.create ~name:"http" (fun () ->
-      match Angstrom.(parse_only (skip_many RFC2616.request) (`String http_get)) with
+      match Angstrom.(parse_bigstring (skip_many RFC2616.request) http_get) with
       | R.Ok _ -> ()
       | R.Error err -> failwith err);
     make_endian "int64 le" Angstrom.LE.int64;
