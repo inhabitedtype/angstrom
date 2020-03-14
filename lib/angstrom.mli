db927b2533fe8ccb046c0578b9b85b4e8a7cd2fc
Spiros Eliopoulos <seliopou@gmail.com>
2020-03-14 13:53:57 -0400

    pr163: move parsers to the 'basic parser' section of the mli

--- lib/angstrom.mli
+++ lib/angstrom.mli
@@ -126,6 +126,10 @@ val take_till : (char -> bool) -> string t
     This parser does not fail. If [f] returns [true] on the first character, it
     will return the empty string. *)
 
+val consumed : _ t -> string t
+(** [consumed p] runs [p] and returns the contents that were consumed during the
+    parsing as a string *)
+
 val take_bigstring : int -> bigstring t
 (** [take_bigstring n] accepts exactly [n] characters of input and returns them
     as a newly allocated bigstring. *)
@@ -150,6 +154,10 @@ val take_bigstring_till : (char -> bool) -> bigstring t
 
     This parser does not fail. If [f] returns [true] on the first character, it
     will return the empty bigstring. *)
+
+val consumed_bigstring : _ t -> bigstring t
+(** [consumed p] runs [p] and returns the contents that were consumed during the
+    parsing as a bigstring *)
 
 val advance : int -> unit t
 (** [advance n] advances the input [n] characters, failing if the remaining
@@ -291,14 +299,6 @@ val skip_many : _ t -> unit t
 
 val skip_many1 : _ t -> unit t
 (** [skip_many1 p] runs [p] {i one} or more times, discarding the results. *)
-
-val consumed : _ t -> string t
-(** [consumed p] runs [p] and returns the contents that were consumed during the
-    parsing as a string *)
-
-val consumed_bigstring : _ t -> bigstring t
-(** [consumed p] runs [p] and returns the contents that were consumed during the
-    parsing as a bigstring *)
 
 val fix : ('a t -> 'a t) -> 'a t
 (** [fix f] computes the fixpoint of [f] and runs the resultant parser. The
