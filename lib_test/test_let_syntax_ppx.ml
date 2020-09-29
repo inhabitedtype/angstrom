81a21a1626d0c06b9ebd08db2847b306401fcc97
Spiros Eliopoulos <seliopou@gmail.com>
2020-09-29 08:00:52 -0400

    pr204: disable mapn test for ppx_let

--- lib_test/test_let_syntax_ppx.ml
+++ lib_test/test_let_syntax_ppx.ml
@@ -11,8 +11,12 @@ let (_ : int t) =
   in
   2
 
+(* [mapn] support was introduced in ppx_let.v0.14.0, which CI does not reliably
+  install. *)
+(*
 let (_ : int t) =
   let%mapn (_ : char) = any_char
   and (_ : string) = string "foo"
   in
   2
+*)
